4c7e1d0048040bdd002f644fe11620d167f70496
Gustavo Rodrigues <dev.gustavor@outlook.com>
2026-05-09 18:21:46 -0300

    feat: notify on resetFilter() when filters applied

--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 import type { FilterForm } from '../components/Filter/Filter';
 
 export function useFilter(defaultValues?: FilterForm) {
@@ -18,8 +19,12 @@ export function useFilter(defaultValues?: FilterForm) {
   const applyFilter = (newFilter: FilterForm) => setAppliedFilter(newFilter);
 
   const resetFilter = () => {
-    reset();
-    setAppliedFilter({ logical: 'AND', filters: [] });
+    if (appliedFilter.filters.length) {
+      reset();
+      setAppliedFilter({ logical: 'AND', filters: [] });
+
+      toast('Os filtros aplicados foram removidos', { position: 'top-right' });
+    }
   };
 
   const filterCount = appliedFilter.filters.length;
